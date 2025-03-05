e770a2b13456f2ae2aaa9726f72289e96d228189
Vu Hieu <vuminhhieubg1@gmail.com>
2025-03-05 22:13:20 +0700

    add paint 2d

---
+++ src/BTVN/Ngay2/Phan3/Paint2D/IShapeO.java
@@ -0,0 +1,9 @@
+package BTVN.Ngay2.Phan3.Paint2D;
+
+public interface IShapeO {
+    public double area();
+
+    public double perimeter();
+
+    public double distance();
+}
---
+++ src/BTVN/Ngay2/Phan3/Paint2D/IShapeT.java
@@ -0,0 +1,9 @@
+package BTVN.Ngay2.Phan3.Paint2D;
+
+public interface IShapeT {
+    public void move(double dx, double dy);
+
+    public void rotate(double alpha);
+
+    public void zoom(double ratio);
+}
