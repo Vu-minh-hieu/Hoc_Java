38c90084698ce11beaada8348dbafe6ec9f95d51
Vu Hieu <vuminhhieubg1@gmail.com>
2025-03-11 02:06:38 +0700

    add test

--- src/BTVN/Ngay2/Phan3/Test.java
+++ src/BTVN/Ngay2/Phan3/Test.java
@@ -2,12 +2,14 @@ package BTVN.Ngay2.Phan3;
 
 import BTVN.Ngay2.Phan3.Paint2D.Circle2D;
 import BTVN.Ngay2.Phan3.Paint2D.IShape;
+import BTVN.Ngay2.Phan3.Paint2D.Line2D;
 import BTVN.Ngay2.Phan3.Paint2D.Point2D;
 
 public class Test {
     public static void main(String[] args) {
         IShape[] list = {new Point2D(10, 10), new Point2D(5, 20),
-                new Circle2D(new Point2D(-10, 2), 6)};
+                new Circle2D(new Point2D(-10, 2), 6),
+                new Line2D(new Point2D(5, 8), new Point2D(3, 6))};
 
         // Operation
         double S = 0;
@@ -17,9 +19,26 @@ public class Test {
         System.out.println(S);
 
         // Transformation
+        /*
         for (IShape shape : list) {
             shape.move(5, 5);
         }
+        */
+
+        // Rotate
+        /*
+        for (IShape shape : list) {
+            shape.rotate(Math.PI / 2);
+        }
+         */
+
+        // Zoom
+        
+        for (IShape shape : list) {
+            shape.zoom(2);
+        }
+
+
         //print all shape
         for (IShape shape : list) {
             System.out.println(shape);
