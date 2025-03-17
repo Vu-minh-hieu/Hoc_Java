ef5566883c2e64c647ce56f9f27b802b40da477d
Vu Hieu <vuminhhieubg1@gmail.com>
2025-03-18 01:36:42 +0700

    add dsach so phuc

--- src/BTVN/Ngay2/Phan4/SoPhuc/DSSP.java
+++ src/BTVN/Ngay2/Phan4/SoPhuc/DSSP.java
@@ -1,5 +1,7 @@
 package BTVN.Ngay2.Phan4.SoPhuc;
 
+import BTVN.Ngay2.Phan1.PhanSo.PhanSo;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,4 +24,46 @@ public class DSSP {
         this.ds = ds;
     }
 
+    //them vao list
+    public void add(SoPhuc p){
+        ds.add(p);
+    }
+
+    //tinh trung binh cong so phuc
+    public SoPhuc TBC(){
+        double sumThuc = 0;
+        double sumAo = 0;
+        int n = ds.size();
+        for (SoPhuc p : ds) {
+            sumThuc += p.getA();
+            sumAo += p.getB();
+        }
+        return new SoPhuc(sumThuc / n, sumAo / n);
+    }
+
+    //lay so phuc co module lon nhat tu list
+    public SoPhuc getMaxModul() {
+        if (ds.isEmpty()) {
+            return null;
+        }
+
+        SoPhuc max = ds.get(0);
+        for (SoPhuc p : ds) {
+            if (p.modulus() > max.modulus()) {
+                max = p;
+            }
+        }
+        return max;
+    }
+
+    //dem so so phuc ko co phan ao
+    public int demSPKoCoPhanAo() {
+        int cnt = 0;
+        for (SoPhuc p : ds) {
+            if (p.getB() == 0) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
 }
--- src/BTVN/Ngay2/Phan4/Test.java
+++ src/BTVN/Ngay2/Phan4/Test.java
@@ -1,5 +1,28 @@
 package BTVN.Ngay2.Phan4;
 
-public class Test {
+import BTVN.Ngay2.Phan4.SoPhuc.SoPhuc;
+import BTVN.Ngay2.Phan4.SoPhuc.DSSP;
 
+public class Test {
+    public static void main (String[] arg){
+        SoPhuc p1 = new SoPhuc(3, 4);
+        SoPhuc p2 = new SoPhuc(5, 6);
+        SoPhuc p3 = new SoPhuc(6, 0);
+        SoPhuc p4 = new SoPhuc(7, 6);
+        SoPhuc p5 = new SoPhuc(5, 0);
+
+        DSSP dssp = new DSSP();
+        dssp.add(p1);
+        dssp.add(p2);
+        dssp.add(p3);
+        dssp.add(p4);
+        dssp.add(p5);
+        System.out.println("Danh sach so phuc: ");
+        for (SoPhuc p : dssp.getDs()){
+            System.out.println(p);
+        }
+        System.out.println("Trung binh cong cac so phuc: " + dssp.TBC());
+        System.out.println("So phuc co modulus lon nhat la: " + dssp.getMaxModul());
+        System.out.println("So so phuc khong co phan ao la: " + dssp.demSPKoCoPhanAo());
+    }
 }
